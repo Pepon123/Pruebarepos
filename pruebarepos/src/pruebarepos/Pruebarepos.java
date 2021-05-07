30257d37f5f4b2f9de1174e938f31d418aea8373
Jose Cordero <Jose Cordero@PEPE2>
2021-05-07 13:01:11 -0500

    unomasynosvamos

---
+++ pruebarepos/src/pruebarepos/Pruebarepos.java
@@ -0,0 +1,16 @@
+
+package pruebarepos;
+
+
+public class Pruebarepos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //esta es una prueba dos
+    }
+    
+}
